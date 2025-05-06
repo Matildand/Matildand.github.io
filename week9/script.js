1dc52a50c04c9c8de637a04fb4c58d31a2d7e615
Matildand <s4007568@student.rmit.edu.au>
2025-05-06 12:22:57 +1000

    week 9

---
+++ week9/script.js
@@ -0,0 +1,14 @@
+const myButton = document.querySelector("#my-button");
+myButton.addEventListener("click", moveInput);
+
+const myInput = document.querySelector("#my-input");
+let clicked = false;
+function moveInput() {
+  if (!clicked) {
+    myInput.style.translate = "30px 40px";
+    clicked = true;
+  } else {
+    myInput.style.translate = "0px 0px";
+    clicked = false;
+  }
+}
